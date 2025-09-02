2a2976f237aa0576ede3b9c455af57e7e4260795
exfeipingli20231012 <147703593+exfeipingli20231012@users.noreply.github.com>
2025-09-02 09:45:21 +0000

    mv namespace to right space

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,3 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
 }
-android{
-    namespace = "com.agent2droid.portal"
-}
